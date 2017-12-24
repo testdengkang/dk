9e2ad865f4719206546d68c2d9b870b3592cdb64
dengk <wydekang@yeah.net>
2017-12-24 17:17:51 +0800

    反射DEMO

--- nged_javaDemo/src/main/java/com/nged/reflect/ReflectionDemo.java
+++ nged_javaDemo/src/main/java/com/nged/reflect/ReflectionDemo.java
@@ -8,26 +8,28 @@ public class ReflectionDemo {
 
     public static void main(String[] args) throws NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException, ClassNotFoundException {
 
+
         //获取对象①
         Student student = new Student(20,"tom");
         Class clazz2 =student.getClass();
-//        ②
+        //获取对象②
         Class clazz = Student.class;
-//        ③
+        //获取对象③
         Class clazz1 = Class.forName("com.nged.reflect.Student");
-//        获取构造函数（带参数的）
+
+        //获取构造函数（带参数的）
         Constructor constructor = clazz.getConstructor(int.class,String.class);
-//        获取无参数的构造器
+        //获取无参数的构造器
         Constructor constructor1 = clazz.getConstructor();
 
-//          获取对象带有参数的方法
+        //获取对象带有参数的方法
         Method method = clazz.getMethod("sayHello",String.class);
-//        获取对象无参数的方法
+        //获取对象无参数的方法
         Method method1 = clazz.getMethod("saySomething");
 
         //初始化有参数的构造器
         Object object = constructor.newInstance(20,"hanmeimei");
-//        执行有参数的方法
+        //执行有参数的方法
         method.invoke(object,"lilei ");
 
         //初始化无参构造器

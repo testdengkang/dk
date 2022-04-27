1e02adcdfa2e58b5eb96577396692a53492577e1
dengkang <dengkanghenu@gmail.com>
2022-04-27 23:28:24 +0800

    hashMap 初始化为大于或等于输入长度的值二的倍数

--- nged_javaDemo/src/main/java/com/nged/hashMapDemo/HashMapTest.java
+++ nged_javaDemo/src/main/java/com/nged/hashMapDemo/HashMapTest.java
@@ -5,11 +5,24 @@ import java.util.LinkedList;
 
 public class HashMapTest {
     public static void main(String[] args){
-       /* HashMap<String,Integer> hashMap = new HashMap<>(32,0.75f);
+        HashMap<String,Integer> hashMap = new HashMap<>(32,0.75f);
         hashMap.put("1",1);
         hashMap.put("2",2);
-        System.out.println(hashMap.size());*/
+        System.out.println(hashMap.size());
 
         System.out.println(4 & 31);
+        getInitSize(9);
+    }
+
+    //获取 大于或等于 m 的 二的倍数值
+    public static void getInitSize(int m){
+        m = m-1;
+        m = m | m>>>1;
+        m = m | m>>>2;
+        m = m | m>>>4;
+        m = m | m>>>8;
+        m |= m>>>16;
+        m = m<0?1: m+1;
+        System.out.println("m == " + m);
     }
 }
